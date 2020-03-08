e09f063874614549a485e454cd67739c2247fa26
Andrey Morozov <jastic7@yandex.ru>
2020-03-08 23:24:30 +0300

    Add solution for Array Partition I task.

---
+++ TwoPointerTechnique/Array Partition I.playground/Contents.swift
@@ -0,0 +1,38 @@
+/*:
+
+# Array Partition I
+
+Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+```
+Input: [1,4,3,2]
+
+Output: 4
+
+Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+```
+
+**Note:**
+
+n is a positive integer, which is in the range of [1, 10000].
+All the integers in the array will be in the range of [-10000, 10000].
+*/
+
+class Solution {
+
+	/// Complexity is O(n*logn)
+    func arrayPairSum(_ nums: [Int]) -> Int {
+		let sorted = nums.sorted()
+		var sum = 0
+		for idx in 0..<sorted.count {
+			if idx % 2 == 0 {
+				sum += sorted[idx]
+			}
+		}
+		return sum
+    }
+}
+
+let solver = Solution()
+solver.arrayPairSum([1,4,3,2])
+
